c0cbb11c7d02d7519224fd371960862495431278
Alejandra C. Araya Díaz <Arayadiaz.ac@gmail.com>
2023-06-11 13:25:53 -0300

    Actualización de models.py
    creación del model para formulario de contacto

--- telovendo3/telovendo3app/models.py
+++ telovendo3/telovendo3app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=100, null=False, blank=False)
+    asunto = models.CharField(max_length=100, null=False, blank=False)
+    mensaje = models.CharField(max_length=1000, null=False, blank=False)
+
+    def __str__(self):
+        return self.nombre
